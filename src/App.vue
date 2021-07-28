4589cf201f75557c9ff364af07af0834e1d87fab
sunzsh <cn.xiaoshan@gmail.com>
2021-07-28 14:34:25 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<button @click="changeBg">点击将第一个设置背景颜色</button>
+<swiper :pagination="true" loop="true" class="mySwiper">
+  <swiper-slide :class="{'bg': item.bg}" v-for="item in datas" :key="item.text">{{item.text}}</swiper-slide>
+  </swiper>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/swiper-bundle.css'
+import "swiper/components/pagination/pagination.min.css"
+import './style.css';
+
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from 'swiper/core';
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+        datas: [
+          { text: 'Slide 1', bg: false },
+          { text: 'Slide 2', bg: false },
+          { text: 'Slide 3', bg: false },
+          { text: 'Slide 4', bg: false }
+        ]
+        
+    };
+  },
+  methods: {
+    changeBg() {
+      this.datas[0].text = "abaababab";
+      this.datas[0].bg = true;
+    }
   }
+  
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+/* 这里需要添加一个带有样式穿的选择器（::v-deep .xxx ） 用来命中被组件复制的item */
+.bg, ::v-deep .bg {
+  background-color: red;
 }
 </style>
